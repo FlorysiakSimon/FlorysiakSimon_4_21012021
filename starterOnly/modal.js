287ebc655a36005cfe5604a6b4d6d300d176bba2
simon <simon.florysiak@gmail.com>
2021-01-25 17:53:14 +0100

    form validate test

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -32,3 +32,32 @@ function closeModal(){
 // close modal event
 close.addEventListener("click", closeModal);
 
+// FORM Elements 
+const firstName = document.getElementById("first");
+const lastName = document.getElementById("last");
+const mail = document.getElementById("email");
+const birthDate = document.getElementById("birthdate");
+const nbTournaments = document.getElementById("quantity");
+const location1 = document.getElementById("location1");
+const location2 = document.getElementById("location2");
+const location3 = document.getElementById("location3");
+const location4 = document.getElementById("location4");
+const location5 = document.getElementById("location5");
+const location6 = document.getElementById("location6");
+
+//check if FORM is valid
+function validate(){
+  if (firstName.value == "") {
+    window.alert("Please enter your name.");
+    firstName.focus();
+    return false;
+  }
+  if (lastName.value == "") {
+    window.alert("Please enter your name.");
+    lastName.focus();
+    return false;
+  }
+  alert("Merci ! Votre réservation a été reçue"); //form valid
+  return true;
+
+}
